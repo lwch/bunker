02e70036bc97d52fb4c9d516199943726fff145a
lwch <lwch748@gmail.com>
2022-07-15 00:38:59 +0800

    修改左侧导航栏样式

--- frontend/app/scripts/views/components/sidebar/index.vue
+++ frontend/app/scripts/views/components/sidebar/index.vue
@@ -1,34 +1,64 @@
 <template>
-<!-- Drawer -->
 <ui-drawer viewport-height>
   <ui-drawer-header>
     <ui-drawer-title>bunker</ui-drawer-title>
   </ui-drawer-header>
   <ui-drawer-content>
-    <ui-nav>
-      <h1>概览</h1>
-      <ui-nav-item href="/#/" active>
-        <ui-icon>dashboard</ui-icon>&nbsp;仪表盘
-      </ui-nav-item>
-      <ui-divider></ui-divider>
+    <ui-list-group>
+      <ui-list-group-subheader>概览</ui-list-group-subheader>
+      <ui-nav>
+        <router-link
+          v-slot="{ href, isActive }"
+          to="/"
+          style="text-decoration:none;">
+          <ui-nav-item :href="href" :active="isActive">
+            <ui-item-first-content>
+              <ui-icon>dashboard</ui-icon>
+            </ui-item-first-content>
+            <ui-item-text-content>仪表盘</ui-item-text-content>
+          </ui-nav-item>
+        </router-link>
+      </ui-nav>
 
-      <h1>用户管理</h1>
-      <ui-nav-item href="/#/users">
-        <ui-icon>person</ui-icon>&nbsp;用户列表
-      </ui-nav-item>
-      <ui-nav-item href="/#/users2">
-        <ui-icon>person</ui-icon>&nbsp;用户列表
-      </ui-nav-item>
-      <ui-divider></ui-divider>
+      <ui-list-divider></ui-list-divider>
 
-      <h1>资产管理</h1>
-      <ui-nav-item href="javascript:void(0)">
-        <ui-icon>computer</ui-icon>&nbsp;资产列表
-      </ui-nav-item>
-      <ui-nav-item href="javascript:void(0)">
-        <ui-icon>tag</ui-icon>&nbsp;标签管理
-      </ui-nav-item>
-    </ui-nav>
+      <ui-list-group-subheader>用户管理</ui-list-group-subheader>
+      <ui-nav>
+        <router-link
+          v-slot="{ href, isActive }"
+          to="/users"
+          style="text-decoration:none;">
+          <ui-nav-item :href="href" :active="isActive">
+            <ui-item-first-content>
+              <ui-icon>person</ui-icon>
+            </ui-item-first-content>
+            <ui-item-text-content>用户列表</ui-item-text-content>
+          </ui-nav-item>
+        </router-link>
+        <router-link
+          v-slot="{ href, isActive }"
+          to="/users2"
+          style="text-decoration:none;">
+          <ui-nav-item :href="href" :active="isActive">
+            <ui-item-first-content>
+              <ui-icon>person</ui-icon>
+            </ui-item-first-content>
+            <ui-item-text-content>用户列表</ui-item-text-content>
+          </ui-nav-item>
+        </router-link>
+      </ui-nav>
+      <ui-list-divider></ui-list-divider>
+
+      <ui-list-group-subheader>资产管理</ui-list-group-subheader>
+      <ui-nav>
+        <ui-nav-item href="javascript:void(0)">
+          <ui-icon>computer</ui-icon>&nbsp;资产列表
+        </ui-nav-item>
+        <ui-nav-item href="javascript:void(0)">
+          <ui-icon>tag</ui-icon>&nbsp;标签管理
+        </ui-nav-item>
+      </ui-nav>
+    </ui-list-group>
   </ui-drawer-content>
 </ui-drawer>
 </template>
@@ -40,11 +70,4 @@ export default {
 </script>
 
 <style scoped>
-a {
-  text-decoration: none;
-  padding-bottom: 5px;
-}
-.router-link-active {
-  text-decoration: none;
-}
 </style>
